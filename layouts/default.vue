c20e2ca3ba31a172bef394830e54da82757a2723
Huynh AT <tienv2i@gmail.com>
2018-03-25 13:17:11 +0700

    Add nuxtjs

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable='md' type='dark' variant='primary'>
+      <b-container>
+        <b-navbar-toggle target='nav_collapse'></b-navbar-toggle>
+        <b-navbar-brand>MIINEWS</b-navbar-brand>
+        <b-collapse is-nav id='nav_collapse'>
+          <b-navbar-nav>
+            <b-nav-item :to='{path: "/"}' exact>HOME</b-nav-item>
+            <b-nav-item :to='{ path: "/todo" }' exact>TODO</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <nuxt/>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container class="mt-3">
+    <b-row>
+      <b-col>
+        <b-jumbotron header="MiiNews Project" lead="MiiNews project created by NuxtJS and BootstrapVue" >
+          <p>For more information visit website</p>
+          <b-btn variant="primary" href="#" v-b-modal.moreInfo>More Info</b-btn>
+        </b-jumbotron>
+      </b-col>
+    </b-row>
+    <b-modal id="moreInfo" title="Information!" ok-only>
+      <p>
+        This site is in development
+      </p>
+      <p>
+        Contact: tienv2i&lt;tienv2i@gmailcom&gt;
+      </p>
+    </b-modal>
+  </b-container>
+</template>
